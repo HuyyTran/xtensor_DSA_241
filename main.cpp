f28e47fdee7d0a7c12fc9f8fee6548e35a718cb2
Huy Tran <huytrn.dev@gmail.com>
2024-10-09 11:42:32 +0700

    add more test

--- main.cpp
+++ main.cpp
@@ -50,24 +50,45 @@ void test_xarray()
   // xt::xtensor<double, 2> b = {{1., 2.}, {3., 4.}};
   // std::cout << b << std::endl;
 
-  xt::xtensor<double, 3> b = {{{11., 22., 33., 5}, {33., 44., 55., 5}, {33., 44., 55., 5}},
-                              {{131., 22., 33., 5}, {5, 343., 44., 55.}, {32., 14., 65., 2}}};
-  auto size = b.size();     // size = 6
-  auto dim = b.dimension(); // dim = 2
-  auto shape = b.shape();   // shape = {2, 3}
-  auto sh0 = b.shape(0);    // sh1 = 3;
-  auto sh1 = b.shape(1);    // sh1 = 3;
-  auto sh2 = b.shape(2);    // sh1 = 3;
-  cout << b << endl;
-  cout << "size = " << size << endl;
-  cout << "dim = " << dim << endl;
-  cout << "shape = " << xt::adapt(shape) << std::endl;
-  cout << "sh0 = " << sh0 << endl;
-  cout << "sh1 = " << sh1 << endl;
-  cout << "sh2 = " << sh2 << endl;
+  // Shape - dimensions - size
+  // xt::xtensor<double, 3> b = {{{11., 22., 33., 5}, {33., 44., 55., 5}, {33., 44., 55., 5}},
+  //                             {{131., 22., 33., 5}, {5, 343., 44., 55.}, {32., 14., 65., 2}}};
+  // auto size = b.size();     // size = 24
+  // auto dim = b.dimension(); // dim = 3
+  // auto shape = b.shape();   // shape = {2, 3, 4}
+  // auto sh0 = b.shape(0);    // sh1 = 2;
+  // auto sh1 = b.shape(1);    // sh1 = 3;
+  // auto sh2 = b.shape(2);    // sh1 = 4;
+  // cout << b << endl;
+  // cout << "size = " << size << endl;
+  // cout << "dim = " << dim << endl;
+  // cout << "shape = " << xt::adapt(shape) << std::endl;
+  // cout << "sh0 = " << sh0 << endl;
+  // cout << "sh1 = " << sh1 << endl;
+  // cout << "sh2 = " << sh2 << endl;
   
-  // xt::xtensor<double, 1> b1 = {11., 22., 33.};
-  // std::cout << b1 << std::endl;
+  xt::xarray<double> a0 = {1., 2., 3., 4., 5., 6.};
+  cout<<"current: "<<endl;
+  cout<<a0<<endl;
+  cout<<"shape = "<<xt::adapt(a0.shape())<<endl;
+  cout<<"dimention= "<<a0.dimension()<<endl;
+  cout<<endl;
+
+  a0.reshape({3,2});
+  cout<<"after 1st reshape: "<<endl;
+  cout<<a0<<endl;
+  cout<<"shape = "<<xt::adapt(a0.shape())<<endl;
+  cout<<"dimention= "<<a0.dimension()<<endl;
+  cout << a0 << std::endl;
+  cout<<endl;
+
+  a0.reshape({2,3});
+  cout<<"after 2nd reshape: "<<endl;
+  cout<<a0<<endl;
+  cout<<"shape = "<<xt::adapt(a0.shape())<<endl;
+  cout<<"dimention= "<<a0.dimension()<<endl;
+  cout << a0 << std::endl;
+  // outputs {{1., 2., 3.}, {4., 5., 6. }}
 
   // xt::xtensor_fixed<double, xt::xshape<2, 2>> c = {{1.1, 2.2}, {3.3, 4.4}};
   // std::cout << c << std::endl;
